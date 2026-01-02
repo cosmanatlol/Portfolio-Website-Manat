b2fe9131c9e1c0c655ab1476889194d6aa5c1227
cosmanatlol <manatomix@gmail.com>
2026-01-02 14:10:40 -0500

    change images

--- projects/script.js
+++ projects/script.js
@@ -45,13 +45,10 @@ function showProjects(projects) {
     let projectsHTML = "";
     
     projects.forEach(project => {
-        console.log(project.image, "=>",
-  `https://cosmanatlol.github.io/Portfolio-Website-Manat/assets/images/projects/${project.image}`
-    );
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}" alt="project" />
+      <img draggable="false" href="https://cosmanatlol.github.io/Portfolio-Website-Manat/assets/images/projects/${project.image}" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
